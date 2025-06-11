3f256bb038b1a11b516c45d48885fb9e7b3c470a
NatalieChurch <nataliechurch12@gmail.com>
2025-06-10 21:01:49 -0500

    Added ProductList Again

---
+++ src/components/ProductDetails.jsx
@@ -0,0 +1,68 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+// import fetchSingleProduct or whatever it's called from the API
+
+function ProductDetails () {
+
+    const {id} = useParams();
+    const [product, setProduct] = useState(null);
+    const navigate = useNavigate();
+    const token = localStorage.getItem("token");
+        // change what const token = if needed
+
+
+useEffect(() => {
+    const getProduct = async () => {
+        try {
+            const data = await fetchSingleProduct(id);
+            setProduct(data);
+        } catch (err) {
+            console.error(err)
+        }
+    };
+    getProduct();
+}, [id]);
+
+
+const handleOrder = async (productId) =>{
+    try {
+    // add code to handle a product order to put it on the user page
+    } catch (err) {
+        console.error(err);
+        alert ("Order was not possible.")
+    }
+};
+
+
+return (
+
+    <div>
+        {product && (
+            <div key={product.id} id="singleProductDisplay">
+                <h1>{product.title}</h1>                
+                <h2>{product.price}</h2>
+                <img src={product.image} alt={product.title} />
+                <h2>{product.description}</h2>
+
+            {token && (
+                <>
+                    <button
+                        onClick = {() => handleOrder(product.id)}>
+                        Order Now
+                    </button>
+                </>
+            )}
+
+            <button
+                onClick = {() => navigate(`/`)}> 
+                Return
+            </button>
+
+            </div>
+        )}
+    </div>
+
+)
+}
+
+export default ProductDetails
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,80 @@
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+// add import from wherever we're calling the fetchAllProducts 
+
+function ProductList () {
+
+    const [products, setProducts] = useState([])
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredResults, setFilteredResults] = useState([]);
+    const navigate = useNavigate();
+
+
+useEffect(() => {
+    const apiCall = async () => {
+        const res = await fetchAllProducts()
+            //change above name if necessary
+        setProducts(res)
+        setFilteredResults(res);
+    }
+        apiCall();
+}, []);
+
+
+useEffect(() => {
+    if (searchTerm.trim() === "") {
+        setFilteredResults([]);
+    } else {
+        const lowerSearch = searchTerm.toLowerCase();
+        const filtered = products.filter(product => product.title.toLowerCase().includes(lowerSearch));
+            // possibly change product.title if name isn't the right word there
+        setFilteredResults(filtered);
+    }
+}, [searchTerm, products]);
+
+
+return (
+
+<div>
+
+    <div className="searchBar">
+        <h1>Search for a Product</h1>
+        <input 
+            id = "searchInput"
+            type = "text"
+            placeholder = "Enter search term"
+            value = {searchTerm}
+            onChange = {e => setSearchTerm(e.target.value)}
+        />
+        <div id="searchResults">
+            {filteredResults.map(product => (
+                <h3 key={product.id}>{product.title}</h3>
+            ))}
+        </div>
+    </div>
+
+    <div className="productDisplay">
+            {products &&
+                products.map((products) => 
+                    <div key={products.id}>
+                        <h1>{products.title}</h1>
+                        <img src={products.image} alt={product.title}/>
+                        <button 
+                            onClick={() => navigate(`/product/${products.id}`)} >
+                            See Product Details
+                        </button>
+                            {/* Change route if necessary */}
+                    </div>
+                )
+            }
+    </div>
+
+</div>
+
+
+
+
+    )
+}
+
+export default ProductList
